8770855b6051f68e87184271944b409ef5df8dc0
eduardo.lima <eduardo.lima@mirante.net.br>
2018-09-22 10:38:24 -0300

    Adicionado serviços externos

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,20 +1,9 @@
 angular.module('starter')
-.controller('ListagemController', function($scope) {
+.controller('ListagemController', function($scope, CarroService) {
 
-	$scope.listaDeCarros = [
-								{"nome" : "BMW 120i", "preco" : 70000},
-								{"nome" : "Onix 1.6", "preco" : 35000},
-								{"nome" : "Fiesta 2.0", "preco" : 52000},
-								{"nome" : "C3 1.0", "preco" : 22000},
-								{"nome" : "Uno Fire", "preco" : 11000},
-								{"nome" : "Sentra 2.0", "preco" : 53000},
-								{"nome" : "Astra Sedan", "preco" : 39000},
-								{"nome" : "Vectra 2.0 Turbo", "preco" : 37000},
-								{"nome" : "Hilux 4x4", "preco" : 90000},
-								{"nome" : "Montana Cabine dupla", "preco" : 57000},
-								{"nome" : "Outlander 2.4" ,"preco" : 99000},
-								{"nome" : "Fusca 1500", "preco" : 6000},
-							];
+	CarroService.obterCarros().then(function(dados) {
+		$scope.listaDeCarros = dados;
+	});
 
 });
 
@@ -41,18 +30,37 @@ angular.module('starter')
 });
 
 angular.module('starter')
-.controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state) {
+.controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state, CarroService) {
 
 	$scope.carroFinalizado = angular.fromJson($stateParams.carro);
 
+	$scope.pedido = {};
+
 	$scope.finaliarPedido = function () {
 
-		$ionicPopup.alert({
-		    title: 'Parabéns!',
-		    template: 'Você acaba de comprar um carro.'
-	    }).then(function () {
-	    	$state.go('listagem');
-	    });
+		var pedidoFinalizado = {
+			params : {
+				carro : $scope.carroFinalizado.nome,
+				preco : $scope.carroFinalizado.preco,
+				nome : $scope.pedido.nome,
+				endereco : $scope.pedido.endereco,
+				email : $scope.pedido.email
+			}
+		}
+
+		CarroService.salvarPedido(pedidoFinalizado).then(function() {
+			$ionicPopup.alert({
+			    title: 'Parabéns!',
+			    template: 'Você acaba de comprar um carro.'
+		    }).then(function () {
+		    	$state.go('listagem');
+		    });
+		}, function() {
+			$ionicPopup.alert({
+			    title: 'Erro!',
+			    template: 'Campos obrigatórios.'
+		    });
+		})
 
 	}
 
---
+++ www/js/service.js
@@ -0,0 +1,23 @@
+angular.module('starter')
+.service('CarroService', function($http) {
+
+	var url = 'http://aluracar.herokuapp.com/';
+
+	return {
+		obterCarros : obterCarros,
+		salvarPedido : salvarPedido
+	}
+
+	function obterCarros()  {
+		return $http.get(url).then(function(response) {
+			return response.data;
+		});
+	}
+
+	function salvarPedido(pedido)  {
+		return $http.get(url + 'salvarpedido', pedido).then(function(response) {
+			return response;
+		});
+	}
+
+});
